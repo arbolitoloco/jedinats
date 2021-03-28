d2c707a5d0c5bac6ff3a7d499a8e335a8be0b550
Laura Rocha Prado <lauraprado@asu.edu>
2021-03-28 11:31:58 -0700

    adds tailwind, species list, specimen table

--- js/index.js
+++ js/index.js
@@ -1,29 +1,43 @@
 const studentGallery = document.getElementById("student-gallery");
 
-function addStudentGallery(data) {
-  data.forEach((item) => {
-    let card = document.createElement("div");
-    card.className = "card";
-    let img = document.createElement("img");
-    img.className = "card-media";
-    img.src = item.img;
-    let content = document.createElement("div");
-    content.className = "card-content";
-    let title = document.createElement("h5");
-    title.className = "card-title";
-    title.innerText = item.name;
-    let text = document.createElement("p");
-    text.innerText = item.shortbio;
-    let btn = document.createElement("button");
-    btn.className = "card-action primary";
-    btn.innerText = "View Page";
-    card.appendChild(img);
-    content.appendChild(title);
-    content.appendChild(text);
-    content.appendChild(btn);
-    card.appendChild(content);
-    studentGallery.appendChild(card);
-  });
+/**
+ * Creates student gallery
+ * Pulls data from JSON in data folder
+ */
+function addStudentGallery() {
+  fetch("./data/students.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        let card = document.createElement("div");
+        card.className = "card";
+        let img = document.createElement("img");
+        img.className = "card-media";
+        img.src = item.img;
+        let content = document.createElement("div");
+        content.className = "card-content";
+        let title = document.createElement("h5");
+        title.className = "card-title";
+        title.innerText = item.name;
+        let text = document.createElement("p");
+        text.innerText = item.shortbio;
+        let btn = document.createElement("button");
+        btn.className = "card-action primary";
+        btn.innerText = "View Page";
+        btn.onclick = function () {
+          window.location = `/profile/${item.id}.html`;
+        };
+        card.appendChild(img);
+        content.appendChild(title);
+        content.appendChild(text);
+        content.appendChild(btn);
+        card.appendChild(content);
+        studentGallery.appendChild(card);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-getStudents();
+addStudentGallery();
--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+/**
+ * Reads students information from JSON file in data folder
+ */
 function getStudents() {
-  fetch("./data/students.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // append data
-      addStudentGallery(data);
+  let students = fetch("./data/students.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data;
     })
     .catch(function (error) {
       console.log(error);
@@ -15,7 +16,7 @@ function getStudents() {
 /**
  * Creates Leaflet specimens map
  * Dependencies: Papaparser and jQuery in head
- * @param {*} filename
+ * @param {String} filename full name of csv inside data folder
  */
 function addMap(filename) {
   // Set up initial map center and zoom level
@@ -59,7 +60,7 @@ function addMap(filename) {
   boundsArr = [];
 
   // Read markers data from data.csv
-  $.get(`../data/example.csv`, function (csvString) {
+  $.get(`../data/${filename}`, function (csvString) {
     // Use PapaParse to convert string to array of objects
     var data = Papa.parse(csvString, {
       header: true,
@@ -84,4 +85,125 @@ function addMap(filename) {
   map.attributionControl.setPrefix(
     'Based on <a href="https://github.com/HandsOnDataViz/leaflet-map-csv" target="_blank">Leaflet Map Csv</a>'
   );
+}
+
+/**
+ * Gets list of taxa in csv
+ * Dependencies: Papaparser and jQuery in head
+ * @param {String} filename full name of csv inside data folder
+ */
+function getTaxa(filename) {
+  let taxaContainer = document.getElementById("taxa");
+  $.get(`../data/${filename}`, function (csvString) {
+    // Use PapaParse to convert string to array of objects
+    let data = Papa.parse(csvString, {
+      header: true,
+      dynamicTyping: true,
+    }).data;
+
+    let kingdomsTemp = [];
+    // // For each row in data, get kingdom
+    // // Output unique names in a list
+    data.forEach((row) => {
+      kingdomsTemp.push(row.kingdom);
+    });
+    let kingdoms = onlyUnique(kingdomsTemp).sort();
+    // For each unique taxon, add it in "Taxa List" section
+    kingdoms.forEach((k) => {
+      let ul = document.createElement("ul");
+      let gName = document.createElement("h3");
+      gName.innerText = k;
+      taxaContainer.appendChild(gName);
+      taxaContainer.appendChild(ul);
+      // for each scientific name, add it to a sciname array inside kingdom
+      let scinamesArr = [];
+      data.forEach((item) => {
+        // console.log(k, item.kingdom);
+        if (k == item.kingdom) {
+          // push item to scinamesArr
+          scinamesArr.push(
+            `<em>${item.scientificName}</em> ${item.scientificNameAuthorship}`
+          );
+        }
+      });
+      let scinames = onlyUnique(scinamesArr).sort();
+      scinames.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerHTML = item;
+        ul.appendChild(li);
+      });
+    });
+  });
+}
+
+/**
+ * Helper method to filter array keeping only unique values
+ * @param {Array} list of items to be filtered
+ * @returns {Array} unique list of items
+ */
+function onlyUnique(list) {
+  let unique = [...new Set(list)];
+  return unique;
+}
+
+function getSpecimens(filename) {
+  let specimensContainer = document.getElementById("specimens");
+  $.get(`../data/${filename}`, function (csvString) {
+    // Use PapaParse to convert string to array of objects
+    let results = Papa.parse(csvString, {
+      header: true,
+      dynamicTyping: true,
+    });
+    let data = results.data;
+    let table = document.createElement("table");
+    table.className = "table-auto";
+    table.style = "width:100%; padding-top: 1em;  padding-bottom: 1em;";
+    let headers = results.meta["fields"];
+
+    headers.forEach((col) => {
+      table.insertRow();
+      let newCol = table.rows[0].insertCell();
+      newCol.textContent = col;
+    });
+
+    data.forEach((row) => {
+      table.insertRow();
+      Object.keys(row).forEach((key, i) => {
+        let newCell = table.rows[table.rows.length - 1].insertCell();
+        newCell.textContent = row[key];
+      });
+    });
+    // table.className = "table-auto";
+    // let thead = table.createTHead();
+
+    // let thRow = thead.insertRow();
+    // let tbody = table.createTBody();
+    // let tbRow = tbody.insertRow();
+
+    // headers.forEach((item) => {
+    //   let th = document.createElement("th");
+    //   th.innerText = item;
+    //   thRow.appendChild(th);
+    // });
+
+    // data.forEach(row => {
+    //   let td = document.createElement("td");
+    //   td.innerText =
+    // });
+
+    specimensContainer.append(table);
+  });
+}
+
+function arrayToTable(tableData) {
+  var table = $("<table></table>");
+  $(tableData).each(function (i, rowData) {
+    var row = $("<tr></tr>");
+    $(rowData).each(function (j, cellData) {
+      row.append($("<td>" + cellData + "</td>"));
+    });
+    table.append(row);
+  });
+  console.log(table);
+  return table;
 }
