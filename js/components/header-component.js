2db857e6fa162aeef7dc53aecc3d4e2d7629eeed
Laura Rocha Prado <lauraprado@asu.edu>
2021-05-17 15:27:47 -0700

    updates about reference

--- js/components/header-component.js
+++ js/components/header-component.js
@@ -8,7 +8,6 @@ class Header extends HTMLElement {
     <header>
       <nav x-data="{show:false}" class="flex items-center justify-between flex-wrap bg-black p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
           <span class="font-semibold text-xl tracking-tight">ASU JEDI Naturalists</span>
         </div>
         <div class="block md:hidden">
@@ -19,7 +18,7 @@ class Header extends HTMLElement {
         <div @click.away="show = false" :class="{ 'block': show, 'hidden': !show }" class="w-full block flex-grow md:flex md:justify-end md:w-auto">
           <div>
             <a href="/" class="block md:inline-block text-sm px-4 py-2 leading-none rounded text-green-400 font-semibold border-2 border-transparent hover:border-2 hover:border-green-400 hover:underline mt-4 md:mt-0">Home</a>
-            <a href="/about" class="block md:inline-block text-sm px-4 py-2 leading-none rounded text-green-400 font-semibold border-2 border-transparent hover:border-2 hover:border-green-400 hover:underline mt-4 md:mt-0">About</a>
+            <a href="/about.html" class="block md:inline-block text-sm px-4 py-2 leading-none rounded text-green-400 font-semibold border-2 border-transparent hover:border-2 hover:border-green-400 hover:underline mt-4 md:mt-0">About</a>
           </div>
         </div>
       </nav>
